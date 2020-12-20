8a45c60f5941dc412a7b2c6cad050cdd5c21ffaa
DJ1TJOO <t.ferb1@gmail.com>
2020-12-20 14:40:14 +0100

    Merge branch 'master' of https://github.com/DJ1TJOO/shapez.io-multiplayer

--- src/js/savegame/multiplayer_savegame.js
+++ src/js/savegame/multiplayer_savegame.js
@@ -1,16 +1,8 @@
-import { ReadWriteProxy } from "../core/read_write_proxy";
 import { ExplainedResult } from "../core/explained_result";
 import { SavegameSerializer } from "./savegame_serializer";
 import { BaseSavegameInterface } from "./savegame_interface";
 import { createLogger } from "../core/logging";
-import { globalConfig } from "../core/config";
 import { getSavegameInterface, savegameInterfaces } from "./savegame_interface_registry";
-import { SavegameInterface_V1001 } from "./schemas/1001";
-import { SavegameInterface_V1002 } from "./schemas/1002";
-import { SavegameInterface_V1003 } from "./schemas/1003";
-import { SavegameInterface_V1004 } from "./schemas/1004";
-import { SavegameInterface_V1005 } from "./schemas/1005";
-import { SavegameInterface_V1006 } from "./schemas/1006";
 import { Savegame } from "./savegame";
 
 const logger = createLogger("savegame");
