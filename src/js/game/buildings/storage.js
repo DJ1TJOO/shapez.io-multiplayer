3b02647656a9bcc143a2069c9801772b619c4c7a
DJ1TJOO <t.ferb1@gmail.com>
2021-03-11 20:37:34 +0100

    Storage variant

--- src/js/game/buildings/storage.js
+++ src/js/game/buildings/storage.js
@@ -1,109 +1,17 @@
-import { formatBigNumber } from "../../core/utils";
 import { enumDirection, Vector } from "../../core/vector";
-import { T } from "../../translations";
 import { ItemAcceptorComponent } from "../components/item_acceptor";
 import { ItemEjectorComponent } from "../components/item_ejector";
 import { StorageComponent } from "../components/storage";
 import { enumPinSlotType, WiredPinsComponent } from "../components/wired_pins";
 import { Entity } from "../entity";
 import { MetaBuilding } from "../meta_building";
-import { defaultBuildingVariant } from "../meta_building_variant";
-import { GameRoot } from "../root";
-import { enumHubGoalRewards } from "../tutorial_goals";
+import { DefaultStorageVariant } from "./variants/storage";
 
-const storageSize = 5000;
+export const storageSize = 5000;
 
 export class MetaStorageBuilding extends MetaBuilding {
     constructor() {
         super("storage");
-    }
-
-    /**
-     * @param {string} variant
-     */
-    getSilhouetteColor(variant) {
-        return MetaStorageBuilding.silhouetteColors[variant]();
-    }
-
-    /**
-     * @param {string} variant
-     */
-    getIsRemovable(variant) {
-        return MetaStorageBuilding.isRemovable[variant]();
-    }
-
-    /**
-     * @param {string} variant
-     */
-    getIsRotateable(variant) {
-        return MetaStorageBuilding.isRotateable[variant]();
-    }
-
-    /**
-     * @param {GameRoot} root
-     */
-    getAvailableVariants(root) {
-        const variants = MetaStorageBuilding.avaibleVariants;
-
-        let available = [];
-        for (const variant in variants) {
-            if (variants[variant](root)) available.push(variant);
-        }
-
-        return available;
-    }
-
-    /**
-     * Returns the edit layer of the building
-     * @param {GameRoot} root
-     * @param {string} variant
-     * @returns {Layer}
-     */
-    getLayer(root, variant) {
-        // @ts-ignore
-        return MetaStorageBuilding.layerByVariant[variant](root);
-    }
-
-    /**
-     * @param {string} variant
-     */
-    getDimensions(variant) {
-        return MetaStorageBuilding.dimensions[variant]();
-    }
-
-    /**
-     * @param {string} variant
-     */
-    getShowLayerPreview(variant) {
-        return MetaStorageBuilding.layerPreview[variant]();
-    }
-
-    /**
-     * @param {GameRoot} root
-     * @param {string} variant
-     * @returns {Array<[string, string]>}
-     */
-    getAdditionalStatistics(root, variant) {
-        return MetaStorageBuilding.additionalStatistics[variant](root);
-    }
-
-    /**
-     * @param {number} rotation
-     * @param {number} rotationVariant
-     * @param {string} variant
-     * @param {Entity} entity
-     * @returns {Array<number>|null}
-     */
-    getSpecialOverlayRenderMatrix(rotation, rotationVariant, variant, entity) {
-        let matrices = MetaStorageBuilding.overlayMatrices[variant](entity, rotationVariant);
-        return matrices ? matrices[rotation] : null;
-    }
-
-    /**
-     * @param {string} variant
-     */
-    getRenderPins(variant) {
-        return MetaStorageBuilding.renderPins[variant]();
     }
 
     /**
@@ -113,154 +21,63 @@ export class MetaStorageBuilding extends MetaBuilding {
     setupEntityComponents(entity) {
         MetaStorageBuilding.setupEntityComponents.forEach(func => func(entity));
     }
-
-    /**
-     * @param {Entity} entity
-     * @param {number} rotationVariant
-     * @param {string} variant
-     */
-    updateVariants(entity, rotationVariant, variant) {
-        MetaStorageBuilding.componentVariations[variant](entity, rotationVariant);
-    }
-
-    static setupEntityComponents = [
-        entity =>
-            // Required, since the item processor needs this.
-            entity.addComponent(
-                new ItemEjectorComponent({
-                    slots: [
-                        {
-                            pos: new Vector(0, 0),
-                            direction: enumDirection.top,
-                        },
-                        {
-                            pos: new Vector(1, 0),
-                            direction: enumDirection.top,
-                        },
-                    ],
-                })
-            ),
-        entity =>
-            entity.addComponent(
-                new ItemAcceptorComponent({
-                    slots: [
-                        {
-                            pos: new Vector(0, 1),
-                            directions: [enumDirection.bottom],
-                        },
-                        {
-                            pos: new Vector(1, 1),
-                            directions: [enumDirection.bottom],
-                        },
-                    ],
-                })
-            ),
-        entity =>
-            entity.addComponent(
-                new StorageComponent({
-                    maximumStorage: storageSize,
-                })
-            ),
-        entity =>
-            entity.addComponent(
-                new WiredPinsComponent({
-                    slots: [
-                        {
-                            pos: new Vector(1, 1),
-                            direction: enumDirection.right,
-                            type: enumPinSlotType.logicalEjector,
-                        },
-                        {
-                            pos: new Vector(0, 1),
-                            direction: enumDirection.left,
-                            type: enumPinSlotType.logicalEjector,
-                        },
-                    ],
-                })
-            ),
-    ];
-
-    static dimensions = {
-        [defaultBuildingVariant]: () => new Vector(2, 2),
-    };
-
-    static silhouetteColors = {
-        [defaultBuildingVariant]: () => "#bbdf6d",
-    };
-
-    static isRemovable = {
-        [defaultBuildingVariant]: () => true,
-    };
-
-    static isRotateable = {
-        [defaultBuildingVariant]: () => true,
-    };
-
-    static additionalStatistics = {
-        /**
-         * @param {*} root
-         * @returns {Array<[string, string]>}
-         */
-        [defaultBuildingVariant]: root => [
-            [T.ingame.buildingPlacement.infoTexts.storage, formatBigNumber(storageSize)],
-        ],
-    };
-
-    static overlayMatrices = {
-        [defaultBuildingVariant]: (entity, rotationVariant) => null,
-    };
-
-    static avaibleVariants = {
-        [defaultBuildingVariant]: root => root.hubGoals.isRewardUnlocked(enumHubGoalRewards.reward_storage),
-    };
-
-    static layerByVariant = {
-        [defaultBuildingVariant]: root => "regular",
-    };
-
-    static layerPreview = {
-        [defaultBuildingVariant]: () => "wires",
-    };
-
-    static renderPins = {
-        [defaultBuildingVariant]: () => true,
-    };
-
-    static componentVariations = {
-        [defaultBuildingVariant]: (entity, rotationVariant) => {
-            entity.components.ItemEjector.setSlots([
-                {
-                    pos: new Vector(0, 0),
-                    direction: enumDirection.top,
-                },
-                {
-                    pos: new Vector(1, 0),
-                    direction: enumDirection.top,
-                },
-            ]);
-            entity.components.ItemAcceptor.setSlots([
-                {
-                    pos: new Vector(0, 1),
-                    directions: [enumDirection.bottom],
-                },
-                {
-                    pos: new Vector(1, 1),
-                    directions: [enumDirection.bottom],
-                },
-            ]);
-            entity.components.Storage.maximumStorage = storageSize;
-            entity.components.WiredPins.setSlots([
-                {
-                    pos: new Vector(1, 1),
-                    direction: enumDirection.right,
-                    type: enumPinSlotType.logicalEjector,
-                },
-                {
-                    pos: new Vector(0, 1),
-                    direction: enumDirection.left,
-                    type: enumPinSlotType.logicalEjector,
-                },
-            ]);
-        },
-    };
 }
+
+MetaStorageBuilding.variants = [DefaultStorageVariant];
+
+MetaStorageBuilding.setupEntityComponents = [
+    entity =>
+        // Required, since the item processor needs this.
+        entity.addComponent(
+            new ItemEjectorComponent({
+                slots: [
+                    {
+                        pos: new Vector(0, 0),
+                        direction: enumDirection.top,
+                    },
+                    {
+                        pos: new Vector(1, 0),
+                        direction: enumDirection.top,
+                    },
+                ],
+            })
+        ),
+    entity =>
+        entity.addComponent(
+            new ItemAcceptorComponent({
+                slots: [
+                    {
+                        pos: new Vector(0, 1),
+                        directions: [enumDirection.bottom],
+                    },
+                    {
+                        pos: new Vector(1, 1),
+                        directions: [enumDirection.bottom],
+                    },
+                ],
+            })
+        ),
+    entity =>
+        entity.addComponent(
+            new StorageComponent({
+                maximumStorage: storageSize,
+            })
+        ),
+    entity =>
+        entity.addComponent(
+            new WiredPinsComponent({
+                slots: [
+                    {
+                        pos: new Vector(1, 1),
+                        direction: enumDirection.right,
+                        type: enumPinSlotType.logicalEjector,
+                    },
+                    {
+                        pos: new Vector(0, 1),
+                        direction: enumDirection.left,
+                        type: enumPinSlotType.logicalEjector,
+                    },
+                ],
+            })
+        ),
+];
---
+++ src/js/game/buildings/variants/storage.js
@@ -0,0 +1,91 @@
+import { formatBigNumber, generateMatrixRotations } from "../../../core/utils";
+import { enumDirection, Vector } from "../../../core/vector";
+import { T } from "../../../translations";
+import { enumPinSlotType } from "../../components/wired_pins";
+import { Entity } from "../../entity";
+import { defaultBuildingVariant, MetaBuildingVariant } from "../../meta_building_variant";
+import { GameRoot } from "../../root";
+import { enumHubGoalRewards } from "../../tutorial_goals";
+import { storageSize } from "../storage";
+
+export class DefaultStorageVariant extends MetaBuildingVariant {
+    constructor(metaBuilding) {
+        super(defaultBuildingVariant, metaBuilding);
+    }
+
+    getIsAvailable(root) {
+        return root.hubGoals.isRewardUnlocked(enumHubGoalRewards.reward_storage);
+    }
+
+    /**
+     * Should return the dimensions of the building
+     */
+    getDimensions() {
+        return new Vector(2, 2);
+    }
+
+    /**
+     * Should return additional statistics about this building
+     * @param {GameRoot} root
+     * @returns {Array<[string, string]>}
+     */
+    getAdditionalStatistics(root) {
+        return [[T.ingame.buildingPlacement.infoTexts.storage, formatBigNumber(storageSize)]];
+    }
+
+    /**
+     * Whether to show a preview of the layer when placing the building
+     */
+    getShowLayerPreview() {
+        return "wires";
+    }
+
+    /**
+     * Should return a silhouette color for the map overview or null if not set
+     * @param {number} rotationVariant
+     */
+    getSilhouetteColor(rotationVariant) {
+        return "#bbdf6d";
+    }
+
+    /**
+     * Should update the entity to match the given variants
+     * @param {Entity} entity
+     * @param {number} rotationVariant
+     */
+    updateVariants(entity, rotationVariant) {
+        entity.components.ItemEjector.setSlots([
+            {
+                pos: new Vector(0, 0),
+                direction: enumDirection.top,
+            },
+            {
+                pos: new Vector(1, 0),
+                direction: enumDirection.top,
+            },
+        ]);
+        entity.components.ItemAcceptor.setSlots([
+            {
+                pos: new Vector(0, 1),
+                directions: [enumDirection.bottom],
+            },
+            {
+                pos: new Vector(1, 1),
+                directions: [enumDirection.bottom],
+            },
+        ]);
+        entity.components.Storage.maximumStorage = storageSize;
+        entity.components.WiredPins.setSlots([
+            {
+                pos: new Vector(1, 1),
+                direction: enumDirection.right,
+                type: enumPinSlotType.logicalEjector,
+            },
+            {
+                pos: new Vector(0, 1),
+                direction: enumDirection.left,
+                type: enumPinSlotType.logicalEjector,
+            },
+        ]);
+    }
+}
